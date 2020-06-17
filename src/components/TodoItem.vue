897e3d7d6cfbe0e3dea657211de68c64e71595a2
sgabdullin <sergey.gabdullin@centre-it.com>
2020-06-17 18:55:31 +0500

    Bootstrap was added

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,10 +1,10 @@
 <template>
-    <li>
+    <li class="d-flex justify-content-start align-items-center">
         <input type="checkbox" v-model="todo.complited"/>
-        <strong>{{todo.id}}</strong>
-        <span v-if="!todo.complited">{{ todo.title }}</span>
-        <span v-else style="text-decoration: line-through">{{ todo.title }}</span>
-        <button>X</button>
+        <strong class="ml-3">{{todo.id}}</strong>
+        <span class="ml-3" v-if="!todo.complited">{{ todo.title }}</span>
+        <span class="ml-3" v-else style="text-decoration: line-through">{{ todo.title }}</span>
+        <button class="ml-auto">X</button>
     </li>
 </template>
 <script>
@@ -17,3 +17,8 @@ export default {
    }
 }
 </script>
+<style>
+    li {
+        width: 300px;
+    }
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <ul>
+    <div class="todoList d-flex flex-column">
+        <ul class="mx-auto d-flex flex-column align-items-start">
             <TodoItem v-for="todo in todos"
                       v-bind:todo="todo"
                       :key="todo.id"
@@ -18,3 +18,10 @@
         }
     }
 </script>
+<style scoped>
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+</style>
