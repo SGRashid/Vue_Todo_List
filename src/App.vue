5ac60de13eb17042ab347099c454ff9078c0df91
sgabdullin <sergey.gabdullin@centre-it.com>
2020-06-17 18:09:36 +0500

    Props was added

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>Todo app</h1>
     <hr>
-    <todo-list></todo-list>
+    <TodoList v-bind:todos="todos" />
   </div>
 </template>
 
@@ -11,6 +11,17 @@ import TodoList from '@/components/TodoList';
 
 export default {
   name: 'App',
+    data() {
+      return {
+          todos: [
+              { id: 0, title: 'Купить пиво', complited: false },
+              { id: 1, title: 'Купить чипсы', complited: false },
+              { id: 2, title: 'Купить табак для кальяна', complited: false },
+              { id: 3, title: 'Купить сосиски', complited: false },
+              { id: 4, title: 'Купить хлеб', complited: false },
+          ]
+      }
+    },
   components: {
       TodoList
   }
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,20 @@
 <template>
     <div>
         <ul>
-            <li>
-                todo 1
-            </li>
+            <TodoItem v-for="todo in todos"
+                      v-bind:todo="todo"
+                      :key="todo.id"
+            />
         </ul>
     </div>
 </template>
+
+<script>
+    import TodoItem from '@/components/TodoItem';
+    export default {
+        props: ['todos'],
+        components: {
+            TodoItem
+        }
+    }
+</script>
