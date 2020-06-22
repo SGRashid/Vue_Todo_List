f4966cde42fb18c185af183456a9c31eb56ce27b
sgabdullin <sergey.gabdullin@centre-it.com>
2020-06-22 17:32:50 +0500

    VUE-TEST-add-filters: Добавил фильтр

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -2,7 +2,7 @@
     <li class="d-flex justify-content-start align-items-center w-100">
         <input type="checkbox" v-model="todo.complited"/>
         <strong class="ml-3">{{index + 1}}</strong>
-        <span class="ml-3 text-left" v-bind:class="{complited: todo.complited}">{{ todo.title }}</span>
+        <span class="ml-3 text-left" v-bind:class="{complited: todo.complited}">{{ todo.title | uppercase }}</span>
         <button class="btn btn-outline-danger border-0 ml-auto"
                 v-on:click="$emit('remove-todo', todo.id)"
         >&#10006;</button>
@@ -16,7 +16,12 @@ export default {
            required: true
        },
        index: Number
-   }
+   },
+    filters: {
+       uppercase(value) {
+           return value.toUpperCase();
+       }
+    }
 }
 </script>
 <style>
